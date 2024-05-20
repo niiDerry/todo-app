df51f9867a1f8022bc8c6376f5b5cbef49bf3b38
Nii Derry <derricktagoe@gmail.com>
2024-05-20 13:11:08 +0000

    inline styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Todo from "./components/Todo";
+import InlineComponent from "./components/InlineComponent";
 
 export default function App() {
   return (
     <div className="App">
-      <Todo />
+      {/* <Todo /> */}
+      <InlineComponent />
     </div>
   );
 }
---
+++ src/components/InlineComponent.jsx
@@ -0,0 +1,9 @@
+const heading = { color: "blue", fontSize: "132px" };
+
+export default function InlineComponent() {
+  return (
+    <div>
+      <h1 style={heading}>Inline component</h1>
+    </div>
+  );
+}
